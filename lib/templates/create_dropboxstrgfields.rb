895736e8c485d6d80a71f514cc0a446b5f3d501a
Raul Roman <rroman@libresoft.es>
2012-10-05 13:50:18 +0200

    Adding table instertion on migration

--- lib/templates/create_dropboxstrgfields.rb
+++ lib/templates/create_dropboxstrgfields.rb
@@ -2,10 +2,14 @@ class CreateDropboxstrgfields < ActiveRecord::Migration
   def up
     add_column :cloudstrgusers, :dropbox_akey, :string 
     add_column :cloudstrgusers, :dropbox_asecret, :string 
+    
+    Cloudstrglist.create :plugin_name => "dropbox"
   end
 
   def down
     remove_column :cloudstrgusers, :dropbox_akey
     remove_column :cloudstrgusers, :dropbox_asecret
+
+    Cloudstrglist.delete_all :plugin_name => "dropbox"
   end
 end
